6ad0ea04c830eb4dc61a911a40417fe1fd498b70
Vijay Krishna Maroju <vijaymaroju.vm@gmail.com>
2024-11-08 17:27:02 +0530

    Added App component to components folder and moved App.js, App.module.css there.

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      
+    </div>
+  );
+}
+
+export default App;
