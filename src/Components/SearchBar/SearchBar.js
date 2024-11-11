f0575bc815b2f1ebbffe08d4328f26327d08ac9d
Vijay Krishna Maroju <vijaymaroju.vm@gmail.com>
2024-11-11 19:59:51 +0530

    Added useState hooks in SearchBar Component to manage the search inputs.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
 
 const sortByOptions = {
@@ -8,10 +8,48 @@ const sortByOptions = {
 };
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchLocation, setSearchLocation] = useState('');
+  const [sortingOption, setSortingOption] = useState('Best Match');
+
+  const getSortByClass = (sortByOption) => {
+    if (sortingOption === sortByOption) {
+      return styles.active;
+    }
+    return "";
+  };
+
+  const handleSortByChange = (sortByOption) => {
+    setSortingOption(sortByOption);
+  };
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const handleLocationChange = (event) => {
+    setSearchLocation(event.target.value);
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log('Searching Yelp with Pizza, Brooklyn, best_match');
+  };
+
   const renderSortByOptions = () => {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return (
+        <li
+          className={getSortByClass(sortByOptionValue)}
+          key={sortByOptionValue}
+          onClick={() => {
+            handleSortByChange(sortByOptionValue);
+          }}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   };
 
@@ -20,13 +58,15 @@ const SearchBar = () => {
       <div className={styles.SearchBarSortOptions}>
         <ul>{renderSortByOptions()}</ul>
       </div>
-      <div className={styles.SearchBarFields}>
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
-      </div>
-      <div className={styles.SearchBarSubmit}>
-        <button>Let's Go</button>
-      </div>
+      <form onSubmit={handleSearch}>
+        <div className={styles.SearchBarFields}>
+          <input placeholder="Search Businesses" onChange={handleSearchTermChange}/>
+          <input placeholder="Where?" onChange={handleLocationChange}/>
+        </div>
+        <div className={styles.SearchBarSubmit}>
+          <button type='submit'>Let's Go</button>
+        </div>
+      </form>
     </div>
   );
 };
